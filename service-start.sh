1ed52f9384993580a2dac1f34c8f65b0d7f95aeb
DJviolin <kerozin.joe@gmail.com>
2015-12-14 19:43:10 +0100

    refact

--- service-start.sh
+++ service-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fleetctl submit /home/core/work/lemp/lemp.service && fleetctl start lemp.service; fleetctl journal -follow=true -lines=50 lemp
+fleetctl submit ./lemp.service && fleetctl start lemp.service; fleetctl journal -follow=true -lines=50 lemp
