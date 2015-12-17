39585e35da2e52148655cc69178a7763c1a05014
DJviolin <kerozin.joe@gmail.com>
2015-12-17 17:28:30 +0100

    refact

--- docker-compose-1.5.2-coreos-installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Releases: https://github.com/docker/compose/releases
-# Instructions: https://docs.docker.com/compose/install/
-sudo su
-mkdir -p /opt/bin
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
-chmod +x /opt/bin/docker-compose
-exit
--- install.sh
+++ install.sh
@@ -4,32 +4,33 @@
 MYSQL_PASS="password"
 # openssl rand -base64 37 | sed -e 's/^\(.\{37\}\).*/\1/g'
 
-USER="500"
-SUPERUSER="0"
+# set -e making the commands if they were like &&
+set -e
 
-sudo -u ${USER} echo -e "Installing docker-compose from GitHub Latest release..." \
-\
-&& sudo -u ${SUPERUSER} mkdir -p /opt/bin \
-&& sudo -u ${SUPERUSER} curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose \
-&& sudo -u ${SUPERUSER} chmod +x /opt/bin/docker-compose \
-&& sudo -u ${USER} echo -e "docker-compose installed, verifying..." \
-&& sudo -u ${USER} docker-compose -v \
-\
-&& sudo -u ${USER} echo -e "Creating folder structure..." \
-&& sudo -u ${USER} mkdir -p ~/mysql ~/sqlbackup ~/work/lemp ~/www \
-\
-&& sudo -u ${USER} echo -e "Cloning git repo into \"~/work/lemp\"..." \
-&& sudo -u ${USER} git clone https://github.com/DJviolin/LEMP.git ~/work/lemp \
-\
-&& sudo -u ${USER} echo -e "Showing working directory..." \
-&& sudo -u ${USER} ls -al ~/work/lemp \
-\
-&& sudo -u ${USER} echo -e "Starting docker images and containers generation..." \
-&& sudo -u ${USER} echo -e "\
+echo -e "Installing docker-compose from GitHub Latest release..."
+
+mkdir -p ~/bin
+curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > ~/bin/docker-compose
+chmod +x ~/bin/docker-compose
+export PATH="~/bin:$PATH"
+echo -e "docker-compose installed, verifying..."
+docker-compose -v
+
+echo -e "Creating folder structure..."
+mkdir -p ~/mysql ~/sqlbackup ~/work/lemp ~/www
+
+echo -e "Cloning git repo into \"~/work/lemp\"..."
+git clone https://github.com/DJviolin/LEMP.git ~/work/lemp
+
+echo -e "Showing working directory..."
+ls -al ~/work/lemp
+
+echo -e "Starting docker images and containers generation..."
+echo -e "\
 # Set MySQL Root Password\n\
-MYSQL_ROOT_PASSWORD=${MYSQL_PASS}" > ~/work/lemp/mariadb/mariadb.env \
-&& sudo -u ${USER} cat ~/work/lemp/mariadb/mariadb.env \
-#&& sudo -u ${USER} docker-compose build ~/work/lemp \
-\
-&& sudo -u ${USER} echo -e "LEMP stack has built...\nRun the service with ./service-start.sh command." \
-&& sudo -u ${USER} echo -e "All done! Exiting..."
+MYSQL_ROOT_PASSWORD=`openssl rand -base64 37 | sed -e 's/^\(.\{37\}\).*/\1/g'`" > ~/work/lemp/mariadb/mariadb.env
+cat ~/work/lemp/mariadb/mariadb.env
+#docker-compose build ~/work/lemp
+
+echo -e "LEMP stack has built...\nRun the service with ./service-start.sh command." \
+echo -e "All done! Exiting..."
