205654520ce895caaf978c687165a84583c367c7
DJviolin <kerozin.joe@gmail.com>
2015-12-18 16:31:18 +0100

    Installer script provided

--- install.sh
+++ install.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 
+#TODO: regex - https://github.com/docker/compose/issues/2555
+# sed -r 's/.*compose\/releases\/tag\/(([0-9]\.){2}[0-9])/\1/'
+#LATEST_TAG="https://github.com/docker/compose/releases/latest"
+
 # set -e making the commands if they were like &&
 set -e
 
 # TODO: check if docker-compose is installed with if else
-echo -e "Installing docker-compose from GitHub Latest release:"
-mkdir -p ~/bin
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > ~/bin/docker-compose
-chmod +x ~/bin/docker-compose
-export PATH="~/bin:$PATH"
-echo -e "\nDocker-compose installed, verifying:"
-docker-compose -v
+echo -e "Installing docker-compose from GitHub Master release channel:\n(For non-Nightly, Stable releases please visit their official GitHub page)"
+mkdir -p $HOME/bin
+export PATH="$HOME/bin:$PATH"
+if hash docker-compose 2>/dev/null; then
+  echo -e "\nDocker-compose already installed on your system, skipping step & verifying version:"
+  docker-compose -v
+else
+  #curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > ~/bin/docker-compose
+  #curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64 > ~/bin/docker-compose
+  #curl -L https://github.com/docker/compose/releases/download/$LATEST_TAG/docker-compose-Linux-x86_64 > ~/bin/docker-compose
+  curl -L https://dl.bintray.com/docker-compose/master/docker-compose-`uname -s`-`uname -m` > $HOME/bin/docker-compose
+  chmod +x $HOME/bin/docker-compose
+  echo -e "\nDocker-compose installed, verifying version:"
+  docker-compose -v
+fi
 
 echo -e "\nCreating folder structure:"
-mkdir -p ~/server/mysql ~/server/sqlbackup ~/server/lemp ~/server/www
-echo -e "Done!"
+mkdir -p $HOME/server/mysql $HOME/server/sqlbackup $HOME/server/lemp $HOME/server/www
+echo -e "\
+  $HOME/server/mysql\n\
+  $HOME/server/sqlbackup\n\
+  $HOME/server/lemp\n\
+  $HOME/server/www\n\
+Done!"
 
-echo -e "\nCloning git repo into \"~/server/lemp\":"
-cd ~/server/lemp
-git clone https://github.com/DJviolin/LEMP.git ~/server/lemp
-echo -e "\nShowing working directory..."
-ls -al ~/server/lemp
+if test "$(ls -A "$HOME/server/lemp")"; then
+  echo -e "\n$HOME/server/lemp directory is not empty!\n\nYou have to remove everything here with\n\"$ rm -rf $HOME/server/lemp/\" command and try again to run this script!\n\nScript failed to run. Exiting..."
+  exit 1
+else
+  echo -e "\nCloning git repo into \"$HOME/server/lemp\":"
+  cd $HOME/server/lemp
+  git clone https://github.com/DJviolin/LEMP.git $HOME/server/lemp
+  echo -e "\nShowing working directory..."
+  ls -al $HOME/server/lemp
+fi
 
 echo -e "\nCreating additional files for the stack:"
 
@@ -28,8 +50,8 @@ echo -e "\nCreating additional files for the stack:"
 # http://stackoverflow.com/questions/4937792/using-variables-inside-a-bash-heredoc
 # http://stackoverflow.com/questions/17578073/ssh-and-environment-variables-remote-and-local
 
-echo -e "\nCreating: ~/server/lemp/docker-compose-eof.yml\n"
-cat <<EOF > ~/server/lemp/docker-compose-eof.yml
+echo -e "\nCreating: $HOME/server/lemp/docker-compose-eof.yml\n"
+cat <<EOF > $HOME/server/lemp/docker-compose-eof.yml
 cadvisor:
   image: google/cadvisor:latest
   container_name: lemp_cadvisor
@@ -96,10 +118,10 @@ nginx:
   volumes_from:
     - php
 EOF
-cat ~/server/lemp/docker-compose-eof.yml
+cat $HOME/server/lemp/docker-compose-eof.yml
 
-echo -e "\nCreating: ~/server/lemp/lemp-eof.service\n"
-cat <<EOF > ~/server/lemp/lemp-eof.service
+echo -e "\nCreating: $HOME/server/lemp/lemp-eof.service\n"
+cat <<EOF > $HOME/server/lemp/lemp-eof.service
 [Unit]
 Description=LEMP
 After=etcd.service
@@ -110,7 +132,7 @@ Requires=docker.service
 TimeoutStartSec=0
 #KillMode=none
 ExecStartPre=-/usr/bin/docker cp lemp_mariadb:/var/lib/mysql $HOME/server/sqlbackup
-ExecStartPre=-/bin/bash -c '/usr/bin/tar -zcvf $HOME/server/sqlbackup/sqlbackup_$$(date +%%Y-%%m-%%d_%%H-%%M-%%S)_ExecStartPre.tar.gz $HOME/server/sqlbackup/mysql --remove-files'
+ExecStartPre=-/bin/bash -c '/usr/bin/tar -zcvf $HOME/server/sqlbackup/sqlbackup_\$\$(date +%%Y-%%m-%%d_%%H-%%M-%%S)_ExecStartPre.tar.gz $HOME/server/sqlbackup/mysql --remove-files'
 ExecStartPre=-/opt/bin/docker-compose --file $HOME/server/lemp/docker-compose.yml kill
 ExecStartPre=-/opt/bin/docker-compose --file $HOME/server/lemp/docker-compose.yml rm --force
 ExecStart=/opt/bin/docker-compose --file $HOME/server/lemp/docker-compose.yml up --force-recreate
@@ -118,21 +140,21 @@ ExecStartPost=/usr/bin/etcdctl set /LEMP Running
 ExecStop=/opt/bin/docker-compose --file $HOME/server/lemp/docker-compose.yml stop
 ExecStopPost=/usr/bin/etcdctl rm /LEMP
 ExecStopPost=-/usr/bin/docker cp lemp_mariadb:/var/lib/mysql $HOME/server/sqlbackup
-ExecStopPost=-/bin/bash -c 'tar -zcvf $HOME/server/sqlbackup/sqlbackup_$$(date +%%Y-%%m-%%d_%%H-%%M-%%S)_ExecStopPost.tar.gz $HOME/server/sqlbackup/mysql --remove-files'
+ExecStopPost=-/bin/bash -c 'tar -zcvf $HOME/server/sqlbackup/sqlbackup_\$\$(date +%%Y-%%m-%%d_%%H-%%M-%%S)_ExecStopPost.tar.gz $HOME/server/sqlbackup/mysql --remove-files'
 Restart=always
 #RestartSec=30s
 
 [X-Fleet]
 Conflicts=lemp.service
 EOF
-cat ~/server/lemp/lemp-eof.service
+cat $HOME/server/lemp/lemp-eof.service
 
 echo -e "\n\
 # Set MySQL Root Password\n\
-MYSQL_ROOT_PASSWORD=`openssl rand -base64 37 | sed -e 's/^\(.\{37\}\).*/\1/g'`" > ~/server/lemp/mariadb/mariadb.env > ~/server/mysql-root-password.txt
-cat ~/server/mysql-root-password.txt
+MYSQL_ROOT_PASSWORD=`openssl rand -base64 37 | sed -e 's/^\(.\{37\}\).*/\1/g'`" > $HOME/server/lemp/mariadb/mariadb.env > $HOME/server/mysql-root-password.txt
+cat $HOME/server/mysql-root-password.txt
 
 cd ~
 
-echo -e "\nLEMP stack has successfully built!\n\nRun docker-compose with:\n  $ docker-compose build ~/server/lemp\nRun the systemd service with:\n  $ cd ~/server/lemp && ./service-start.sh\nStop the systemd service with:\n  $ cd ~/server/lemp && ./service-stop.sh"
+echo -e "\nLEMP stack has successfully built!\n\nRun docker-compose with:\n  $ docker-compose build $HOME/server/lemp\nRun the systemd service with:\n  $ cd $HOME/server/lemp && ./service-start.sh\nStop the systemd service with:\n  $ cd $HOME/server/lemp && ./service-stop.sh"
 echo -e "\nAll done! Exiting..."
