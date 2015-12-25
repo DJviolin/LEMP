6fca7c22e02b853ccc05e08f43de3bfa7f65cdbc
DJviolin <kerozin.joe@gmail.com>
2015-12-25 19:32:08 +0100

    chown

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ cadvisor:
   image: google/cadvisor:latest
   container_name: lemp_cadvisor
   ports:
-    - "8082:8080"
+    - "8083:8080"
   volumes:
     - "/:/rootfs:ro"
     - "/var/run:/var/run:rw"
