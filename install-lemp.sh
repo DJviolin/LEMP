f258d16d31177d71ac7bd9931061c3fbe05b9e60
DJviolin <kerozin.joe@gmail.com>
2016-01-15 15:00:16 +0100

    refact

--- install-lemp.sh
+++ install-lemp.sh
@@ -117,11 +117,6 @@ nginx:
     - ./nginx/etc/nginx/nginx.conf:/etc/nginx/nginx.conf:ro
   volumes_from:
     - php
-#app:
-#  build: ./app
-#  container_name: lemp_app
-#  links:
-#    - nginx
 EOF
 cat $REPO_DIR/docker-compose.yml
 
