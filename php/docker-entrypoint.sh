43cb557ee890b5aa749ffa7f3889dea8157de3ba
DJviolin <kerozin.joe@gmail.com>
2016-01-18 22:36:50 +0100

    refact

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -5,6 +5,6 @@ echo PID1 > /dev/null
 
 #Stay in foreground mode, don’t daemonize.
 #/usr/sbin/cron -f
-#/usr/sbin/cron
+/usr/sbin/cron
 
 /usr/local/php7/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php7/etc/php-fpm.conf
