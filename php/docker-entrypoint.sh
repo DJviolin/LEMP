dbc438db974a3313812aac1b10d95ca9414f1cb2
DJviolin <kerozin.joe@gmail.com>
2016-01-21 16:14:51 +0100

    reworked

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo PID1 > /dev/null
 
 #Stay in foreground mode, don’t daemonize.
-#/usr/sbin/cron -f &
-/usr/sbin/cron
+/usr/sbin/cron -f &
+#/usr/sbin/cron
 
 /usr/local/php7/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php7/etc/php-fpm.conf
