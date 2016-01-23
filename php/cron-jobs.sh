6d074421bab719fa692a6a8c33f3b1e8ef989945
DJviolin <kerozin.joe@gmail.com>
2016-01-23 21:00:27 +0100

    refact

--- php/cron-jobs.sh
+++ php/cron-jobs.sh
@@ -11,6 +11,6 @@ while true; do
   #/usr/bin/curl -m 299 http://10.0.2.2/pussybnb.com/wp-cron.php?doing_wp_cron=$(date +\%s.\%N) >> /var/log/wp-cron.log 2>&1
 
   #/usr/local/php7/bin/php /var/www/pussybnb.com/wp-content/plugins/wp-cron-control/wp-cron-control.php http://127.0.0.1/pussybnb.com 48acdc227b8c6c3e2e82fc6a5cdbd771 >> /var/log/wp-cron.log
-  /usr/bin/curl -m 299 "http://127.0.0.1/pussybnb.com/wp-cron.php?doing_wp_cron&48acdc227b8c6c3e2e82fc6a5cdbd771" >> /var/log/wp-cron.log 2>&1
+  /usr/bin/curl -m 299 "http://10.0.2.2/pussybnb.com/wp-cron.php?doing_wp_cron&48acdc227b8c6c3e2e82fc6a5cdbd771" >> /var/log/wp-cron.log 2>&1
   sleep 5m
 done
