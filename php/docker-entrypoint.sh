8b0507c96a077bc59e6867cc1eeef145d44a8553
DJviolin <kerozin.joe@gmail.com>
2016-01-25 11:06:01 +0100

    refact

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo PID1 > /dev/null
 
 # Run script in the background (this is not daemonized)
+chmod +x /cron-jobs.sh
 /cron-jobs.sh &
 
 /usr/local/php7/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php7/etc/php-fpm.conf
